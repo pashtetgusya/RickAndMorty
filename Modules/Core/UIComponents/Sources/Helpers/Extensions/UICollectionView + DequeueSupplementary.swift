c6a8efe6c35c5258aab3ee8e58c45cd37e831d07
Яровой Павел Александрович <yarovoi.p.98@gmail.com>
2026-03-22 12:47:52 +0700

    Add collection header extensions

--- Modules/Core/UIComponents/Sources/Helpers/Extensions/UICollectionView + DequeueSupplementary.swift
+++ Modules/Core/UIComponents/Sources/Helpers/Extensions/UICollectionView + DequeueSupplementary.swift
@@ -4,6 +4,24 @@ import UIKit
 
 public extension UICollectionView {
     
+    /// Выполняет получение повторно используемого хедера коллекции указанного типа.
+    ///
+    /// Если хедер не зарегистрирован или не может быть приведен к указанному типу
+    /// выполняется вызов `fatalError` с соответствующим сообщением.
+    /// - Parameter indexPath: индекс повторно используемого хедера коллекции.
+    /// - Returns: поврторно используемый хедер коллекции.
+    func dequeueHeader<T: UICollectionReusableView>(for indexPath: IndexPath) -> T {
+        guard
+            let header = dequeueReusableSupplementaryView(
+                ofKind: UICollectionView.elementKindSectionHeader,
+                withReuseIdentifier: T.reuseIdentifier,
+                for: indexPath
+            ) as? T
+        else { fatalError("header \(T.self) is not registered in collection") }
+        
+        return header
+    }
+    
     /// Выполняет получение повторно используемого футера коллекции указанного типа.
     ///
     /// Если футер не зарегистрирован или не может быть приведен к указанному типу
--- Modules/Core/UIComponents/Sources/Helpers/Extensions/UICollectionView + RegisterSupplementary.swift
+++ Modules/Core/UIComponents/Sources/Helpers/Extensions/UICollectionView + RegisterSupplementary.swift
@@ -4,6 +4,16 @@ import UIKit
 
 public extension UICollectionView {
     
+    /// Выполняет регистрацию хедера для последующего использования в коллекции.
+    /// - Parameter header: класс хедера, который необходимо зарегистрировать.
+    func registerHeader(_ header: UICollectionReusableView.Type) {
+        register(
+            header.self,
+            forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: header.reuseIdentifier
+        )
+    }
+    
     /// Выполняет регистрацию футера для последующего использования в коллекции.
     /// - Parameter footer: класс футера, который необходимо зарегистрировать.
     func registerFooter(_ footer: UICollectionReusableView.Type) {
